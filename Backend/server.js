3aab512af03b257c8effa8fdb18e59c32dc8ddef
Trentyn Nicholas <trentyn@dcflux.space>
2026-07-15 03:14:41 -0600

    turned off sync

--- Backend/server.js
+++ Backend/server.js
@@ -258,7 +258,7 @@ sequelize.authenticate()
   .then(async () => {
     console.log('✅ Database connected successfully.');
 
-    const ENABLE_SYNC = process.env.SEQUELIZE_SYNC;
+    const ENABLE_SYNC = false;
 
     if (ENABLE_SYNC) {
       await db.sequelize.sync({ alter: true });
